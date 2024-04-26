b425819be6b04276509340e37d878cac753f0290
Havner <100545304+Devinchand@users.noreply.github.com>
2024-04-26 17:55:00 +0700

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -4,11 +4,22 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+let yesBtnClicked = false;
+
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Aaaaa, Terimakasihhhhhh (^.^)"; gif.src = 
-    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";});
+    yesBtnClicked = true;
+    question.innerHTML = " i love you tooo"; gif.src = 
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+
+    resetNoBtnPosition();
+});
+
+    
 
 noBtn.addEventListener("mouseover", () => {
+
+    if (!yesBtnClicked) {
+
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxx = window.innerWidth - noBtnRect.width; 
     const maxY = window.innerHeight - noBtnRect.height;
@@ -18,4 +29,15 @@ noBtn.addEventListener("mouseover", () => {
     
     noBtn.style.left = randomX + "px"; 
     noBtn.style.top = randomY + "px"; 
+
+    question.innerHTML = " Eiitsss Tidak Kena :) ";
+
+    }
+
 });
+
+function resetNoBtnPosition() {
+    noBtn.style.left = "initial"; // Atau ganti dengan posisi semula yang diinginkan
+    noBtn.style.top = "initial";
+    noBtn.removeEventListener("mouseover", noBtn);
+}
